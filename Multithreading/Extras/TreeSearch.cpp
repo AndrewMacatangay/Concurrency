5b05db453a4709113d5dc97b35f90c5fbdb925fc
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-06-17 17:07:57 -0500

    Created Tree class

--- Multithreading/Extras/TreeSearch.cpp
+++ Multithreading/Extras/TreeSearch.cpp
@@ -1,87 +1,103 @@
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
-#include<thread>
 #include<mutex>
 #include<future>
 using namespace std;
 
-struct Node
+class Tree
 {
-	int value;
-	Node* left;
-	Node* right;
+	private:
 
-	Node(int v) : value(v), left(nullptr), right(nullptr) { }
+	struct Node
+	{
+		int value;
+		Node* left;
+		Node* right;
+
+		Node(int v) : value(v), left(nullptr), right(nullptr) { }
+	};
+
+	Node* head;
+	
+	public:
+
+	Tree(int numNodes)
+	{
+		//srand(time(NULL));
+		head = nullptr;
+
+		for(int x = 0; x < numNodes; x++)
+			insertInOrder(nullptr, head, rand() % numNodes + 1);
+	}
+
+	void _inOrder(Node* cur)
+	{
+		if (!cur)
+			return;
+	
+		_inOrder(cur->left);
+		cout << cur->value << " ";
+		_inOrder(cur->right);
+	}
+
+	void inOrder()
+	{
+		_inOrder(head);
+	}
+
+	void insertInOrder(Node* parent, Node*& cur, int value)
+	{
+		if (!parent && !cur)
+		{
+			cur = new Node(value);
+			return;
+		}
+		else if (!cur)
+		{
+			(parent->value < value ? parent->right : parent->left) = new Node(value);
+			return;
+		}
+		else if (cur->value == value)
+			return;
+
+		cur->value > value ? insertInOrder(cur, cur->left, value) : insertInOrder(cur, cur->right, value);
+	}
+
+	int _search(Node* cur, int value)
+	{
+		if (!cur)
+			return -1;
+		if (cur->value == value)
+			return value;
+	
+		future<int> leftFuture = async(&Tree::_search, this, cur->left, value);
+		future<int> rightFuture = async(&Tree::_search, this, cur->right, value);
+
+		int leftFound = leftFuture.get();
+		int rightFound = rightFuture.get();
+
+		if (leftFound == rightFound)
+			return -1;
+
+		return leftFound > rightFound ? leftFound : rightFound;
+
+		//cur->value > value ? search(cur->left, value) : search(cur->right, value);
+	}
+
+	int search(int value)
+	{
+		return _search(head, value);
+	}
 };
-
-void inOrder(Node* cur)
-{
-	if (!cur)
-		return;
-	
-	inOrder(cur->left);
-	cout << cur->value << " ";
-	inOrder(cur->right);
-}
-
-void insertInOrder(Node* parent, Node*& cur, int value)
-{
-	if (!parent && !cur)
-	{
-		cur = new Node(value);
-		return;
-	}
-	else if (!cur)
-	{
-		(parent->value < value ? parent->right : parent->left) = new Node(value);
-		return;
-	}
-	else if (cur->value == value)
-		return;
-
-	cur->value > value ? insertInOrder(cur, cur->left, value) : insertInOrder(cur, cur->right, value);
-}
-
-int search(Node* cur, int value)
-{
-	if (!cur)
-		return -1;
-	if (cur->value == value)
-		return value;
-	
-	//Add a future here?
-	future<int> leftFuture = async(search, cur->left, value);
-	future<int> rightFuture = async(search, cur->right, value);
-
-	int leftFound = leftFuture.get();
-	int rightFound = rightFuture.get();
-
-	if (leftFound == rightFound)
-		return -1;
-
-	return leftFound > rightFound ? leftFound : rightFound;
-
-	//cur->value > value ? search(cur->left, value) : search(cur->right, value);
-}
 
 int main()
 {
-	//srand(time(NULL));
+	Tree tree(1000);
+	tree.inOrder();
 
-	Node* head = nullptr;
-	
-	for(int x = 0; x < 1000; x++)
-	{
-		int random = rand() % 1000 + 1;
-		//cout << random << " ";
-		insertInOrder(nullptr, head, random);
-	}
-
-	//cout << endl;
-	inOrder(head);
-
-	cout << "Search result: " << search(head, 588) << endl;
+	cout << endl;
+	cout << "Search result: " << tree.search(588) << endl;
 
 	return 0;
 }
