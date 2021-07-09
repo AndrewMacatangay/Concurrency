095b7706eed86be8dcee4b32c7f7228150b0e9e6
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-09 18:08:05 -0500

    A little progress

--- Multithreading/Chapter5/AtomicUDT.cpp
+++ Multithreading/Chapter5/AtomicUDT.cpp
@@ -6,17 +6,42 @@ using namespace std;
 struct Node
 {
 	int value;
-	Node* next;
+	atomic<Node*> next;
+	Node(int v = -1, Node* n = nullptr) : value(v), next(n) { }
 };
+
+atomic<Node*> head(nullptr);
+atomic<Node*> tail(nullptr);
+
+void push_back(int value)
+{
+	if (!head)
+	{
+		head = tail = new Node(value);
+		return;
+	}
+	atomic<Node*> newNode(new Node(value));	
+	tail.load()->next = newNode.load();
+	tail = newNode.load();
+
+}
+
+void print()
+{
+	for(Node* cur = head; cur; cur = cur->next)
+		cout << cur->value << " ";
+	cout << endl;
+}
 
 int main()
 {
 	cout << "Node: " << is_trivially_copyable<Node>::value << endl;
 	cout << "Node to Node: " << is_trivially_copy_assignable<Node>::value << endl;
-	atomic<Node> n;
-	n.store({1, nullptr});
 
-	cout << n.load().value << endl;
+	for(int x = 0; x < 5; x++)
+		push_back(x);
+
+	print();
 
 	return 0;
 }
