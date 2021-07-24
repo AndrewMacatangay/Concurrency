717283a7a98df7df33b873322573d3f36419fffd
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-23 19:28:39 -0500

    Almost done. One bug?

--- Multithreading/Chapter5/AcquireRelease2.cpp
+++ Multithreading/Chapter5/AcquireRelease2.cpp
@@ -4,34 +4,42 @@
 #include <vector>
 using namespace std;
 
-atomic<int> buns(0), patty(0), cheese(0), burgers(0), isReady(-1);
+//Start with some X amount of stock, and decrement it as more burgers
+//are produced. If the consumer is not able to recieve a burger because
+//of the lack of stock, notify the consumer.
+atomic<int> ingredientSet(3), buns(0), patty(0), cheese(0), burgers(0), isReady(-1);
 
 void produce(int orderNumber)
 {
-	buns.fetch_add(1, memory_order_relaxed);
-	patty.fetch_add(1, memory_order_relaxed);
-	cheese.fetch_add(1, memory_order_relaxed);
-	isReady.store(orderNumber, memory_order_release);
+	//buns.fetch_add(1, memory_order_relaxed);
+	//patty.fetch_add(1, memory_order_relaxed);
+	//cheese.fetch_add(1, memory_order_relaxed);
+
+	int stock = ingredientSet.load(memory_order_seq_cst);
+	if(stock)
+		ingredientSet.fetch_sub(1, memory_order_seq_cst);
+	isReady.store(stock ? orderNumber : -1, memory_order_release);
 }
 
 void consume(int orderNumber)
 {
 	int cycles = 0;
-	while(cycles < 100000 && isReady.load(memory_order_acquire) != orderNumber)
+	while(cycles < 150000 && isReady.load(memory_order_acquire) != orderNumber)
 		cycles++;
-	if (cycles != 100000 && 
+	/*if (cycles != 100000 && 
 	    buns.load(memory_order_relaxed) &&
 	    patty.load(memory_order_relaxed) &&
-	    cheese.load(memory_order_relaxed))
+	    cheese.load(memory_order_relaxed))*/
+	    if (cycles != 150000)
 	    {
-		cout << "Success!" << " " << cycles << endl;
-		buns.fetch_sub(1, memory_order_relaxed);
-		patty.fetch_sub(1, memory_order_relaxed);
-		cheese.fetch_sub(1, memory_order_relaxed);
+		//cout << "Success!" << " " << cycles << endl;
+		//buns.fetch_sub(1, memory_order_relaxed);
+		//patty.fetch_sub(1, memory_order_relaxed);
+		//cheese.fetch_sub(1, memory_order_relaxed);
 		burgers.fetch_add(1, memory_order_relaxed);
 	    }
 	else
-		cout << "Failure..." << endl;
+		cout << "Failure..." << " " << orderNumber << endl;
 }
 
 int main()
@@ -42,7 +50,7 @@ int main()
 	for (int x = 0; x < 3; x++)
 		consumers.push_back(thread(consume, x));
 
-	for (int x = 0; x < 2; x++)
+	for (int x = 0; x < 3; x++)
 		producers.push_back(thread(produce, x));
 
 	for (thread& t : consumers)
@@ -51,6 +59,7 @@ int main()
 		t.join();
 
 	cout << "Burgers: "  << burgers << endl;
-	cout << buns << " " << patty << " " << cheese << endl;
+	//cout << buns << " " << patty << " " << cheese << endl;
+	//cout << ingredientSet << endl;
 	return 0;
 }
