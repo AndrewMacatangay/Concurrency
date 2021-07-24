500ae0ff6c1a5b02191698381cc9368b1fd76a53
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-24 16:55:43 -0500

    Back to the drawing board

--- Multithreading/Chapter5/AcquireRelease2.cpp
+++ Multithreading/Chapter5/AcquireRelease2.cpp
@@ -9,37 +9,29 @@ using namespace std;
 //of the lack of stock, notify the consumer.
 atomic<int> ingredientSet(3), buns(0), patty(0), cheese(0), burgers(0), isReady(-1);
 
+//Get rid of a set of ingredients. If there is still more, then return the order number,
+//otherwise 0
 void produce(int orderNumber)
 {
-	//buns.fetch_add(1, memory_order_relaxed);
-	//patty.fetch_add(1, memory_order_relaxed);
-	//cheese.fetch_add(1, memory_order_relaxed);
-
-	int stock = ingredientSet.load(memory_order_seq_cst);
-	if(stock)
-		ingredientSet.fetch_sub(1, memory_order_seq_cst);
-	isReady.store(stock ? orderNumber : -1, memory_order_release);
+	int store = ingredientSet.fetch_sub(1, memory_order_relaxed);
+	isReady.store(store ? orderNumber : -1, memory_order_release);
 }
 
+//Let the cashier wait until its ready. If they waited too long, there are no more
+//sets of ingredients
 void consume(int orderNumber)
 {
+	//Race conditions with "cycles?"
 	int cycles = 0;
-	while(cycles < 150000 && isReady.load(memory_order_acquire) != orderNumber)
+
+	while(cycles != 150000 && isReady.load(memory_order_acquire) != orderNumber)
 		cycles++;
-	/*if (cycles != 100000 && 
-	    buns.load(memory_order_relaxed) &&
-	    patty.load(memory_order_relaxed) &&
-	    cheese.load(memory_order_relaxed))*/
-	    if (cycles != 150000)
-	    {
-		//cout << "Success!" << " " << cycles << endl;
-		//buns.fetch_sub(1, memory_order_relaxed);
-		//patty.fetch_sub(1, memory_order_relaxed);
-		//cheese.fetch_sub(1, memory_order_relaxed);
-		burgers.fetch_add(1, memory_order_relaxed);
-	    }
-	else
-		cout << "Failure..." << " " << orderNumber << endl;
+//Fix here
+	if(cycles != 150000)
+	//if(cycles != 1500000 && ingredientSet.load())	
+		burgers.fetch_add(1);
+	//else
+		//cout << cycles << endl;
 }
 
 int main()
@@ -50,7 +42,7 @@ int main()
 	for (int x = 0; x < 3; x++)
 		consumers.push_back(thread(consume, x));
 
-	for (int x = 0; x < 3; x++)
+	for (int x = 0; x < 2; x++)
 		producers.push_back(thread(produce, x));
 
 	for (thread& t : consumers)
