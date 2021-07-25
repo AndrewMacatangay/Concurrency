b301b79be490efe86de0e9640f1ee3aa3bd3ba13
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-25 18:35:18 -0500

    still debugging

--- Multithreading/Chapter5/AcquireRelease2.cpp
+++ Multithreading/Chapter5/AcquireRelease2.cpp
@@ -7,14 +7,14 @@ using namespace std;
 //Start with some X amount of stock, and decrement it as more burgers
 //are produced. If the consumer is not able to recieve a burger because
 //of the lack of stock, notify the consumer.
-atomic<int> ingredientSet(3), buns(0), patty(0), cheese(0), burgers(0), isReady(-1);
+atomic<int> ingredientSet(3), buns(0), patty(0), cheese(0), burgers(0), isReady(0);
 
 //Get rid of a set of ingredients. If there is still more, then return the order number,
 //otherwise 0
 void produce(int orderNumber)
 {
 	int store = ingredientSet.fetch_sub(1, memory_order_relaxed);
-	isReady.store(store ? orderNumber : -1, memory_order_release);
+	isReady.store(store ? orderNumber : -orderNumber, memory_order_release);
 }
 
 //Let the cashier wait until its ready. If they waited too long, there are no more
@@ -22,16 +22,16 @@ void produce(int orderNumber)
 void consume(int orderNumber)
 {
 	//Race conditions with "cycles?"
-	int cycles = 0;
+	//int cycles = 0;
 
-	while(cycles != 150000 && isReady.load(memory_order_acquire) != orderNumber)
-		cycles++;
-//Fix here
-	if(cycles != 150000)
-	//if(cycles != 1500000 && ingredientSet.load())	
+	//while(cycles != 150000)
+	int ready = isReady.load(memory_order_acquire);
+	for(; ready != orderNumber && ready != -orderNumber; ready = isReady.load(memory_order_acquire));
+
+	if (ready == orderNumber)
 		burgers.fetch_add(1);
-	//else
-		//cout << cycles << endl;
+	else if (ready == -orderNumber)
+		cout << "tset" << endl;
 }
 
 int main()
@@ -39,10 +39,10 @@ int main()
 	vector<thread> consumers;
 	vector<thread> producers;
 
-	for (int x = 0; x < 3; x++)
+	for (int x = 1; x <= 2; x++)
 		consumers.push_back(thread(consume, x));
 
-	for (int x = 0; x < 2; x++)
+	for (int x = 1; x <= 2; x++)
 		producers.push_back(thread(produce, x));
 
 	for (thread& t : consumers)
