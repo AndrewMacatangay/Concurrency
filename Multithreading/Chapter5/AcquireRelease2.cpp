4060a63539c3a0bf5a39ecc44277b7c38cce027c
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-26 18:17:28 -0500

    Finished acq/rel exmaple

--- Multithreading/Chapter5/AcquireRelease2.cpp
+++ Multithreading/Chapter5/AcquireRelease2.cpp
@@ -1,57 +1,47 @@
+/*
+In this program, we demonstrate acquire-release barriers by having a thread that deals with
+writes and another thread that deals with reads. The writing thread will has a release barrier
+and all prior writes to memory will be executed. In the reading thread, the acquire will sync
+with the release and all read/writes will happen after the acquire barrier.
+
+Sidenote: Think of barriers as locks. Operations can be reordered to be under an acquire barrier
+and above a release barrier.
+*/
+
 #include <iostream>
 #include <atomic>
 #include <thread>
 #include <vector>
 using namespace std;
 
-//Start with some X amount of stock, and decrement it as more burgers
-//are produced. If the consumer is not able to recieve a burger because
-//of the lack of stock, notify the consumer.
-atomic<int> ingredientSet(3), buns(0), patty(0), cheese(0), burgers(0), isReady(0);
+atomic<bool> x, y;
+atomic<int> z;
 
-//Get rid of a set of ingredients. If there is still more, then return the order number,
-//otherwise 0
-void produce(int orderNumber)
+void writeXThenY()
 {
-	int store = ingredientSet.fetch_sub(1, memory_order_relaxed);
-	isReady.store(store ? orderNumber : -orderNumber, memory_order_release);
+	x.store(true, memory_order_relaxed);
+	y.store(true, memory_order_release);
 }
 
-//Let the cashier wait until its ready. If they waited too long, there are no more
-//sets of ingredients
-void consume(int orderNumber)
+void readYThenX()
 {
-	//Race conditions with "cycles?"
-	//int cycles = 0;
-
-	//while(cycles != 150000)
-	int ready = isReady.load(memory_order_acquire);
-	for(; ready != orderNumber && ready != -orderNumber; ready = isReady.load(memory_order_acquire));
-
-	if (ready == orderNumber)
-		burgers.fetch_add(1);
-	else if (ready == -orderNumber)
-		cout << "tset" << endl;
+	while(!y.load(memory_order_acquire));
+	if (x.load(memory_order_relaxed))
+		z++;
 }
 
 int main()
 {
-	vector<thread> consumers;
-	vector<thread> producers;
+	x = y = false;
+	z = 0;
 
-	for (int x = 1; x <= 2; x++)
-		consumers.push_back(thread(consume, x));
+	thread a(writeXThenY);
+	thread b(readYThenX);
 
-	for (int x = 1; x <= 2; x++)
-		producers.push_back(thread(produce, x));
+	a.join();
+	b.join();
 
-	for (thread& t : consumers)
-		t.join();
-	for (thread& t : producers)
-		t.join();
+	cout << "Z: " << z << endl;
 
-	cout << "Burgers: "  << burgers << endl;
-	//cout << buns << " " << patty << " " << cheese << endl;
-	//cout << ingredientSet << endl;
 	return 0;
 }
