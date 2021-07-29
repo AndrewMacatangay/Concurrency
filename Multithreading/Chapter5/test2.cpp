21d1d5f3167884f906c6eb9607d0e864ef1035cb
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-29 17:54:05 -0500

    taking a break

--- Multithreading/Chapter5/test2.cpp
+++ Multithreading/Chapter5/test2.cpp
@@ -4,51 +4,40 @@
 #include <thread>
 using namespace std;
 
-atomic<int> counter(0), ingredients(3);
+atomic<int> counter(0);
 vector<int> v;
 
-void produce()
+void produce(int stock)
 {
-	int numElements = ingredients.load(memory_order_relaxed);
-	v.clear();
-	for(int x = 0; x < numElements; x++)
-		v.push_back(x * 10);
-	counter.store(numElements, memory_order_release);
+	v = vector<int>(stock, 0);
+	counter.store(stock, memory_order_release);
 	
 }
 
-void consume()
+void consume(int threadNumber)
 {
 	int index = 0;
-	while(1)
-	{
-		if((index = counter.fetch_sub(1, memory_order_acquire)) <= 0)
-		{
-			if (index < 0)
-				break;
-			continue;
-		}
-		else
-		{
-			cout << v[index - 1] << endl;
-			break;
-		}
-	}
+	while((index = counter.fetch_sub(1, memory_order_acquire)) <= 0);
+	v[index - 1]++;
+	//v.push_back(1);
 }
 
 int main()
 {
 	vector<thread> consumers;
 
-	thread t1(produce);
-
-	for (int x = 0; x < 5; x++)
-		consumers.push_back(thread(consume));
+	thread t1(produce, 7);
+	
+	for (int x = 0; x < 6; x++)
+		consumers.push_back(thread(consume, x + 1));
 
 	t1.join();
 
 	for (thread& t : consumers)
 		t.join();
+
+	for(size_t x = 0; x < v.size(); x++)
+		cout << "Thread " << x + 1 << ": " << v[x] << endl;
 
 	return 0;
 }
