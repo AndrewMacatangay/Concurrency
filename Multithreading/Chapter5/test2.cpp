15785a4a23b3d0b272a556b4090833294832af73
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-07-29 19:15:51 -0500

    debugging

--- Multithreading/Chapter5/test2.cpp
+++ Multithreading/Chapter5/test2.cpp
@@ -10,33 +10,38 @@ vector<int> v;
 void produce(int stock)
 {
 	v = vector<int>(stock, 0);
-	counter.store(stock, memory_order_release);
-	
+	counter.store(stock, memory_order_release);	
 }
 
-void consume(int threadNumber)
+void consume(int stock, int threadNumber)
 {
 	int index = 0;
-	while((index = counter.fetch_sub(1, memory_order_acquire)) <= 0);
-	v[index - 1]++;
-	//v.push_back(1);
+	while((index = counter.fetch_sub(1, memory_order_acquire)) <= 0)
+	{
+		if (index < 0)
+			return;
+		//cout << stock << " " << threadNumber << " " << index << endl;
+	};
+	v[stock - index] = threadNumber;
 }
 
 int main()
 {
 	vector<thread> consumers;
+	int supply = 5;
+	int demand = 6;
 
-	thread t1(produce, 7);
+	thread t1(produce, supply);
 	
-	for (int x = 0; x < 6; x++)
-		consumers.push_back(thread(consume, x + 1));
+	for (int x = 0; x < demand; x++)
+		consumers.push_back(thread(consume, supply, x + 1));
 
 	t1.join();
 
 	for (thread& t : consumers)
 		t.join();
 
-	for(size_t x = 0; x < v.size(); x++)
+	for(int x = 0; x < (demand < supply ? demand : supply); x++)
 		cout << "Thread " << x + 1 << ": " << v[x] << endl;
 
 	return 0;
