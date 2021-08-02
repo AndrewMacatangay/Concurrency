dc32676b416d49161435736083081d22930f60b9
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-08-02 17:06:50 -0500

    Files are fully cleaned

--- Multithreading/Projects/Project2/Client.cpp
+++ Multithreading/Projects/Project2/Client.cpp
@@ -2,22 +2,22 @@
 #include <string>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
 using namespace std;
 
 int main()
 {
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(8080);
+	char buffer[4096];
+
+	struct sockaddr_in addr = {AF_INET, htons(8080)};
 	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
 	connect(clientSocket, (struct sockaddr*) &addr, sizeof(addr));
 
-	char buffer[4096];
 	send(clientSocket, "Hello from Client", 18, 0);
 	cout << "Hello sent from Client" << endl;
 	read(clientSocket, buffer, 4096);
-	printf("%s\n", buffer);
+	cout << buffer << endl;
+	
 	return 0;
 }
--- Multithreading/Projects/Project2/Server.cpp
+++ Multithreading/Projects/Project2/Server.cpp
@@ -6,46 +6,26 @@ using namespace std;
 
 int main()
 {
-	int serverSocket;
+	int serverSocket, opt = 1, sockLen, connection;
+	char buffer[4096];
+
 	if (!(serverSocket = socket(AF_INET, SOCK_STREAM, 0)))
-	{
-		cout << "Failed!" << endl;
-		return 1;
-	}
+	{ cerr << "Failed!" << endl; return 1; }
 
-	int opt = 1;
-	if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	{
-		cout << "Failed!" << endl;
-		return 1;
-	}
+	if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+	{ cerr << "Failed!" << endl; return 1; }
 
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(8080);
-	addr.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in addr = {AF_INET, htons(8080), INADDR_ANY};
+	sockLen = sizeof(addr);
 
 	if (bind(serverSocket, (struct sockaddr*) &addr, sizeof(addr)) < 0)
-	{
-		cout << "Failed!" << endl;
-		return 1;
-	}
+	{ cerr << "Failed!" << endl; return 1; }
 
 	if (listen(serverSocket,10) < 0)
-	{
-		cout << "Failed!" << endl;
-		return 1;
-	}
+	{ cerr << "Failed!" << endl; return 1; }
 
-	int sockLen = sizeof(addr);
-	int socket;
-	if ((socket = accept(serverSocket, (struct sockaddr*) &addr, (socklen_t*) &sockLen)) < 0)
-	{
-		cout << "Failed!" << endl;
-		return 1;
-	}
-
-	char buf[4096];
+	if ((connection = accept(serverSocket, (struct sockaddr*) &addr, (socklen_t*) &sockLen)) < 0)
+	{ cerr << "Failed!" << endl; return 1; }
 
 	/*while(1)
 	{
@@ -55,10 +35,11 @@ int main()
 		cout << "Looped!" << endl;
 	}*/
 
-	read(socket, buf, 4096);
-	printf("%s\n", buf);
-	send(socket, "Hello from Server", 18, 0);
-	printf("Hello sent from Server\n");
+	read(connection, buffer, 4096);
+	cout << buffer << endl;
+	send(connection, "Hello from Server", 18, 0);
+	cout << "Hello sent from Server" << endl;
+
 	close(serverSocket);
 
 	return 0;
