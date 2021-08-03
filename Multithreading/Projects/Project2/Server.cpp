63e21b0fdd95a945cbb99c24cfb21a569245b41c
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-08-03 00:30:59 -0500

    Now compatible with multiple clients!

--- Multithreading/Projects/Project2/Server.cpp
+++ Multithreading/Projects/Project2/Server.cpp
@@ -2,32 +2,13 @@
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <thread>
 #include <unistd.h>
 using namespace std;
 
-int main()
+void echo(int connection)
 {
-	int serverSocket, opt = 1, sockLen, connection;
 	char buffer[4096];
-
-	if (!(serverSocket = socket(AF_INET, SOCK_STREAM, 0)))
-	{ cerr << "Failed!" << endl; return 1; }
-
-	if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-	{ cerr << "Failed!" << endl; return 1; }
-
-	struct sockaddr_in addr = {AF_INET, htons(8080), INADDR_ANY};
-	sockLen = sizeof(addr);
-
-	if (bind(serverSocket, (struct sockaddr*) &addr, sizeof(addr)) < 0)
-	{ cerr << "Failed!" << endl; return 1; }
-
-	if (listen(serverSocket, 10) < 0)
-	{ cerr << "Failed!" << endl; return 1; }
-
-	if ((connection = accept(serverSocket, (struct sockaddr*) &addr, (socklen_t*) &sockLen)) < 0)
-	{ cerr << "Failed!" << endl; return 1; }
-
 	while (buffer[0] != 'a')
 	{
 		//Once you're done using the information from the buffer, clear it
@@ -36,6 +17,42 @@ int main()
 		cout << "Client: " << buffer << endl;
 		send(connection, buffer, bytesReceived + 1, 0);
 	}
+}
+
+void acceptConnections(int serverSocket, sockaddr_in& addr, int& sockLen)
+{
+	for (int connection; 1; )
+	{
+		if ((connection = accept(serverSocket, (struct sockaddr*) &addr, (socklen_t*) &sockLen)) < 0)
+			{ cerr << "Connection failed!\n"; return; }
+		cout << "Connection: " << connection << endl;
+		thread(echo, connection).detach();
+	}
+}
+
+int main()
+{
+	int serverSocket, opt = 1, sockLen;
+
+	if (!(serverSocket = socket(AF_INET, SOCK_STREAM, 0)))
+		{ cerr << "Socket could not be made!\n"; return 1; }
+
+	if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+		{ cerr << "Socket options failed!\n"; return 1; }
+
+	struct sockaddr_in addr = {AF_INET, htons(8080), INADDR_ANY};
+	sockLen = sizeof(addr);
+
+	if (bind(serverSocket, (struct sockaddr*) &addr, sizeof(addr)) < 0)
+		{ cerr << "Binding failed!\n"; return 1; }
+
+	if (listen(serverSocket, 10) < 0)
+		{ cerr << "Listening failed!\n"; return 1; }
+
+	thread(acceptConnections, serverSocket, ref(addr), ref(sockLen)).detach();
+	
+	//Let the server run forever. Might change this to be some escape character
+	while(1);
 
 	close(serverSocket);
 
