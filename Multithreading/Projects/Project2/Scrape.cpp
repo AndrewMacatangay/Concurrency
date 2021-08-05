1277f841a422e86eed00da2fc572e6fe2a9df53e
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-08-05 13:49:36 -0500

    Fixed scraper bug

--- Multithreading/Projects/Project2/Scrape.cpp
+++ Multithreading/Projects/Project2/Scrape.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 int curlWriter(char* data, int size, int nmemb, string* buffer)
@@ -18,26 +20,48 @@ int curlWriter(char* data, int size, int nmemb, string* buffer)
 	return result;
 }
 
+void parsePrice(string curlBuffer, string URL, string ticker)
+{
+	while(1)
+	{
+		CURL* curl = curl_easy_init();
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriter);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &curlBuffer);
+		curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+		curl_easy_perform(curl);
+		curl_easy_reset(curl);
+		//int priceIndex = curlBuffer.find("currentPrice");
+		int priceIndex = curlBuffer.find("regularMarketPrice");
+		//cout << curlBuffer.substr(priceIndex - 1, 50) << endl;
+		string temp = curlBuffer.substr(priceIndex + 20);
+		cout << ticker << ": " << temp.substr(0, temp.find(',')) << endl;
+		this_thread::sleep_for(1s);
+		curlBuffer.clear();
+	}
+}
+
 int main()
 {
 	string ticker;
 	cout << "Input ticker symbol: ";
 	cin >> ticker;
 	transform(ticker.begin(), ticker.end(), ticker.begin(), ::toupper);
-	string URL = "https://finance.yahoo.com/quote/" + ticker;
+	//string URL = "https://finance.yahoo.com/quote/" + ticker;
+	string URL = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=" + ticker;
 	string curlBuffer;
 
-	CURL* curl = curl_easy_init();
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriter);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &curlBuffer);
-	curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
-	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-	curl_easy_perform(curl);
-
+	//CURL* curl = curl_easy_init();
+	//curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriter);
+	//curl_easy_setopt(curl, CURLOPT_WRITEDATA, &curlBuffer);
+	//curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
+	//curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+	//curl_easy_perform(curl);
+	parsePrice(curlBuffer, URL, ticker);
 	//int priceIndex = curlBuffer.find("<span class=\"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)\" data-reactid=\"49\">");
 	
 	//cout << curlBuffer.substr(priceIndex + 74, 6) << endl;
-	int priceIndex = curlBuffer.find("currentPrice");
-	cout << curlBuffer.substr(priceIndex - 1, 50) << endl;
+	//int priceIndex = curlBuffer.find("currentPrice");
+	//cout << curlBuffer.substr(priceIndex - 1, 50) << endl;
 	return 0;
 }
--- Multithreading/Projects/Project2/Scraper.py
+++ Multithreading/Projects/Project2/Scraper.py
@@ -1,15 +1,14 @@
 import bs4
 import requests
 from bs4 import BeautifulSoup
-
-def parsePrice(ticker):
-	URL = 'https://finance.yahoo.com/quote/' + ticker.upper()
+def parsePrice(URL):
 	r = requests.get(URL)
 	soup = bs4.BeautifulSoup(r.text, 'html5lib')
 	price = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find('span').text
 	return price
 
 ticker = input("Enter ticker symbol: ")
-#while True:
-print(parsePrice(ticker))
+URL = 'https://finance.yahoo.com/quote/' + ticker.upper();
+while True:
+	print(parsePrice(URL))
 #sleep
