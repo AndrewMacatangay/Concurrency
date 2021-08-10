d1851e9c8ce45989092a4f9e7fe18865f842fbe0
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-08-10 11:13:01 -0500

    Added more attributes

--- Multithreading/Projects/Project2/Data.h
+++ Multithreading/Projects/Project2/Data.h
@@ -2,7 +2,7 @@ class Data
 {
 	private:
 	string ticker, JSON, buffer;
-	bool isValidTicker;
+	bool isValidTicker, isCrypto;
 	size_t attributeIndex;
 
 	string getAttribute(string attribute)
@@ -21,7 +21,7 @@ class Data
 		}
 		else
 		{
-			buffer.substr(0, buffer.find(','));
+			buffer = buffer.substr(0, buffer.find(','));
 			buffer[buffer.size() - 2] == '.' && &(buffer += '0');
 		}
 
@@ -29,10 +29,24 @@ class Data
 
 	}
 
+	//Let's add a loop for these
+	//Market cap doesnt have a decimal and has more than 1 comma
+	string formatNumber(string fund)
+	{
+		size_t decimalIndex = fund.find('.');
+		for (decimalIndex == string::npos && (decimalIndex = fund.size()); decimalIndex > 3; decimalIndex -= 3)
+			fund.insert(decimalIndex - 3, ",");
+		return fund;
+	}
+
 	public:
+
+	//Any other error checking for valid tickers must be included here
 	Data(string ticker, string curlBuffer) : ticker(ticker), JSON(curlBuffer)
 	{
-		isValidTicker = JSON.find("\"result\":[]") == string::npos;
+		isValidTicker = JSON.find("\"result\":[]") == string::npos &&
+				JSON.find("regularMarketPrice") != string::npos;
+		isCrypto = JSON.find("CRYPTOCURRENCY") != string::npos;
 	}
 	
 	string getBasicInformation()
@@ -41,11 +55,28 @@ class Data
 			return "Ticker symbol not found!";
 
 		string padding(ticker.size() + 2, ' ');
-		string name = getAttribute("shortName");
-		string exchangeName = getAttribute("fullExchangeName");
 
-		return ticker + ": " + name + '\n'
-		              + padding + exchangeName;
+		string name = getAttribute("shortName");
+		string exchangeName = getAttribute(isCrypto ? "quoteSourceName" : "fullExchangeName");
+		
+		string marketPrice = formatNumber(getAttribute("regularMarketPrice"));
+		string marketChange = formatNumber(getAttribute("regularMarketChange"));
+		//Deal with dollar sign here
+		       marketChange.insert((marketChange[0] == '-' ? 1 : 0), "$");
+
+		string marketCap = formatNumber(getAttribute("marketCap"));
+
+		string marketLow = formatNumber(getAttribute("regularMarketDayLow"));
+		string marketHigh = formatNumber(getAttribute("regularMarketDayHigh"));
+
+		string bid = formatNumber(getAttribute("bid"));
+		string ask = formatNumber(getAttribute("ask"));
+
+		return ticker + ": " + name + " (" + exchangeName + ")\n"
+			      + padding + "$" + marketPrice + " (" + marketChange + ")" + "\n"
+			      + padding + "Market Cap: $" + marketCap + "\n"
+			      + padding + "Range: [$" + marketLow + ", $" + marketHigh + "]\n"
+			      + padding + "Bid/Ask: <$" + bid + ", $" + ask + ">";
 	}
 
 	string getPrice()
